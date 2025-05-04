c18ff9257e743fd884ea3c2fbe0a4b902205b43d
safonovartem <safonovartem829@gmail.com>
2025-05-04 17:34:24 +0300

    Сделал классы

---
+++ SAV_Kursovaya_1/Currency.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAV_Kursovaya_1
+{
+    public class Currency
+    {
+        public string Name { get; set; }
+        public List<CurrencyRate> Rates { get; set; }
+
+        public Currency(string name)
+        {
+            Name = name;
+            Rates = new List<CurrencyRate>();
+        }
+
+        public void AddRate(DateTime date, decimal rate)
+        {
+            Rates.Add(new CurrencyRate(date, rate));
+        }
+    }
+}
---
+++ SAV_Kursovaya_1/CurrencyRate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAV_Kursovaya_1
+{
+    public class CurrencyRate
+    {
+        public DateTime Date { get; set; }
+        public decimal Rate { get; set; }
+
+        public CurrencyRate(DateTime date, decimal rate)
+        {
+            Date = date;
+            Rate = rate;
+        }
+    }
+}
