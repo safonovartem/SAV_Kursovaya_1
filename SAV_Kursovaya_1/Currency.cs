b3679c4bcd9e0ecbc5ec73c86a0f2f821a6031e7
safonovartem <safonovartem829@gmail.com>
2025-05-23 12:37:44 +0300

    Написал что нужгно сделать и с чем помочь

--- SAV_Kursovaya_1/Currency.cs
+++ SAV_Kursovaya_1/Currency.cs
@@ -6,20 +6,20 @@ using System.Threading.Tasks;
 
 namespace SAV_Kursovaya_1
 {
-    public class Currency
+    public class Currency // валюта - класс
     {
-        public string Name { get; set; }
-        public List<CurrencyRate> Rates { get; set; }
+        public string Name { get; set; } // название валюты
+        public List<CurrencyRate> Rates { get; set; } // список курсов валюты
 
-        public Currency(string name)
+        public Currency(string name) // конструктор класса
         {
             Name = name;
             Rates = new List<CurrencyRate>();
         }
 
-        public void AddRate(DateTime date, decimal rate)
+        public void AddRate(DateTime date, decimal rate) // добавление курса валюты
         {
-            Rates.Add(new CurrencyRate(date, rate));
+            Rates.Add(new CurrencyRate(date, rate));// добавление нового курса валюты в список
         }
     }
 }
